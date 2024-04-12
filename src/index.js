37f5e474aabbb87d0f634d98d2a4979b54e83770
Ruben Fiszel <ruben@rubenfiszel.com>
2024-04-12 11:36:53 +0200

    update example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "@windmill-labs/windmill-react-sdk/styles";
 import "./style.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+//react 18
+// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import { OpenAPI as ClientOpenAPI } from "windmill-client";
 import { OpenAPI as ComponentsOpenAPI } from "windmill-components/gen/core/OpenAPI";
@@ -14,9 +16,18 @@ ComponentsOpenAPI.BASE = "/api";
 ComponentsOpenAPI.CREDENTIALS = "include";
 ComponentsOpenAPI.WITH_CREDENTIALS = true;
 
-let react = ReactDOM.createRoot(document.getElementById("root"));
-react.render(
+// react 18
+// let react = ReactDOM.createRoot(document.getElementById("root"));
+// react.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// react 17
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
