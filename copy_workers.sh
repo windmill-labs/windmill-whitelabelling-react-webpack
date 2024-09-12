5febf6bc9864225798faab851d44e717ad0790fd
Ruben Fiszel <ruben@rubenfiszel.com>
2024-09-12 22:09:35 +0200

    all

--- copy_workers.sh
+++ copy_workers.sh
@@ -1,3 +1,2 @@
-cp  node_modules/@windmill-labs/windmill-react-sdk/dist/assets/* public/assets/
-mkdir -p assets
-cp  node_modules/@windmill-labs/windmill-react-sdk/dist/assets/* assets/
+ mkdir -p assets/
+ cp  node_modules/@windmill-labs/windmill-react-sdk/dist/assets/extensionHost.worker-UG49ZGFK.js assets/    
