43d31907584086dcb4141ce1ce470ae73aa2fe5b
Ruben Fiszel <ruben@windmill.dev>
2025-07-07 09:36:37 +0000

    react 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "@windmill-labs/windmill-react-sdk/styles";
 import "./style.css";
 import React from "react";
-//react 18
-// import ReactDOM from "react-dom/client";
+// react 18
+import ReactDOM from "react-dom/client";
 // import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
+// react 19
+// import { createRoot } from 'react-dom/client';
 import { OpenAPI as ClientOpenAPI } from "windmill-client";
 import { OpenAPI as ComponentsOpenAPI } from "@windmill-labs/windmill-react-sdk";
 import App from "./App";
@@ -22,20 +23,20 @@ async function initializeClients() {
 await initializeClients();
 
 // react 19
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// react 18
-// let react = ReactDOM.createRoot(document.getElementById("root"));
-// react.render(
+// const root = createRoot(document.getElementById("root"));
+// root.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
 // );
+
+// react 18
+let react = ReactDOM.createRoot(document.getElementById("root"));
+react.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // react 17
 // ReactDOM.render(
